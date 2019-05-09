88d80c2e9c090748b5035d0e30cd44e4e75e3677
naoya-matsumura <naoya.matsumura@jp.fujitsu.com>
2019-05-09 02:57:33 +0000

    create header footer

---
+++ vision_api/src/components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+  <el-header style="text-align: right; width: 100%; font-size: 12px; background-color: #9ff781;">
+    <el-menu :default-active="activeIndex" class="el-menu-header" mode="horizontal" style="background-color: #9ff781; border-style: none;">
+      <el-menu-item index="1"><router-link to="/top">Top</router-link></el-menu-item>
+      <el-menu-item index="2"><router-link to="/info">Info</router-link></el-menu-item>
+      <el-menu-item index="3"><router-link to="/privacy">Privacy Policy</router-link></el-menu-item>
+      <el-menu-item index="4"><router-link to="/contact">Contact</router-link></el-menu-item>
+    </el-menu>
+  </el-header>
+</template>
--- vision_api/src/router/index.js
+++ vision_api/src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+import Top from '@/components/Top'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    // デフォルトの定義（不要なら削除）
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Top',
+      component: Top
+    },
+    // URLパスに対応するコンポーネントを定義
+    {
+      path: '/top',
+      name: 'Top',
+      component: Top
     }
   ]
 })
